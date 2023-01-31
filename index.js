02aebbfddb8c156b52e7289d28015226c1548ecf
Finn Chen <finn.chen.maenhaut@gmail.com>
2023-01-31 14:53:31 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+// let activity = "roller-skate";
+// // function saturdayFun() {
+// // //   switch (activity) {
+// // //     default:
+// // //       return `This Saturday, I want to ${activity}!`;
+// // //     case "bathe my dog":
+// // //       return "This Saturday, I want to bathe my dog!";
+// // //   }
+// // // }
+
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = "*") {
+  return function inner(orig = "special") {
+    return `You are ${flair}${orig}${flair}!`;
+  };
+}
